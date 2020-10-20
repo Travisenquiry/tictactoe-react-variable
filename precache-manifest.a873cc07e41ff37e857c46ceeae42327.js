cbc4b2823979ff2f74ecfe7abc9fed6a12dec7a4
travisenquiry <travisenquiry@gmail.com>
2020-10-20 16:57:39 +0800

    Updates

---
+++ precache-manifest.a873cc07e41ff37e857c46ceeae42327.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8672953a25dd11e625a176752158261b",
+    "url": "/zendesk-assessment/index.html"
+  },
+  {
+    "revision": "aad5bab0e82ae5c83a75",
+    "url": "/zendesk-assessment/static/css/main.6373f2d6.chunk.css"
+  },
+  {
+    "revision": "4e193ba9901f1c32cba5",
+    "url": "/zendesk-assessment/static/js/2.70e7d301.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/zendesk-assessment/static/js/2.70e7d301.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aad5bab0e82ae5c83a75",
+    "url": "/zendesk-assessment/static/js/main.e0aa1389.chunk.js"
+  },
+  {
+    "revision": "abd68d6b610678c1ef04",
+    "url": "/zendesk-assessment/static/js/runtime-main.dea02ebb.js"
+  }
+]);
